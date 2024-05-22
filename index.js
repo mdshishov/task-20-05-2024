22ca28c4060a180047dad10ffb140f87032a344c
student <mdshishov@mail.ru>
2024-05-22 12:36:21 +0300

    classwork 22-05

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
-import * as _ from '@hexlet/immutable-fs-trees';
+import * as t from '@hexlet/immutable-fs-trees';
+import _ from 'lodash';
 
-const tree = _.mkdir('nodejs-package', [
-  _.mkfile('Makefile'),
-  _.mkfile('README.md'),
-  _.mkdir('dist', []),
-  _.mkdir('__tests__', [
-    _.mkfile('half.test.js', { type: 'text/javascript' }),
+const tree = t.mkdir('nodejs-package', [
+  t.mkfile('Makefile'),
+  t.mkfile('README.md'),
+  t.mkdir('dist', []),
+  t.mkdir('__tests__', [
+    t.mkfile('half.test.js', { type: 'text/javascript' }),
   ]),
-  _.mkfile('babel.config.js', { type: 'text/javascript' }),
-  _.mkdir('node_modules', [
-    _.mkdir('@babel', [
-      _.mkdir('cli', [
-        _.mkfile('LICENCE'),
+  t.mkfile('babel.config.js', { type: 'text/javascript' }),
+  t.mkdir('node_modules', [
+    t.mkdir('@babel', [
+      t.mkdir('cli', [
+        t.mkfile('LICENCE'),
       ])
     ]),
   ], { owner: '@root', hidden: false }),
 ], { hidden: true });
 
+const printTree = (tree) => {
+  console.log(t.getName(tree));
+
+  if (t.isFile(tree)) {
+    return;
+  }
+
+  t.getChildren(tree).forEach((child) => printTree(child));
+};
+
+const changeMeta = (tree, param, value) => {
+  const name = t.getName(tree);
+  const newMeta = _.cloneDeep(t.getMeta(tree));
+  newMeta[param] = value;
+
+  if (t.isFile(tree)) {
+    return t.mkfile(name, newMeta);
+  }
+
+  const children = t.getChildren(tree).map((child) => (changeMeta(child, param, value)));
+  return t.mkdir(name, children, newMeta);
+};
+
+const printTreeWithParam = (tree, param) => {
+  const name = t.getName(tree);
+  const value = t.getMeta(tree)[param];
+  console.log(`${name}, ${param}: ${value}`);
+
+  if (t.isFile(tree)) {
+    return;
+  }
+
+  t.getChildren(tree).forEach((child) => printTreeWithParam(child, param));
+};
+
+const countAll = (tree) => {
+  if (t.isFile(tree)) {
+    return 1;
+  }
+  return 1 + _.sum(t.getChildren(tree).map((child) => countAll(child)));
+};
+
+const countFiles = (tree) => {
+  if (t.isFile(tree)) {
+    return 1;
+  }
+  return _.sum(t.getChildren(tree).map((child) => countFiles(child)));
+};
+
+const countDirectories = (tree) => {
+  if (t.isFile(tree)) {
+    return 0;
+  }
+  return 1 + _.sum(t.getChildren(tree).map((child) => countDirectories(child)));
+};
+
 console.log(tree);
+printTree(tree);
+
+const newTree = changeMeta(tree, 'owner', 'me');
+console.log(newTree);
+printTreeWithParam(tree, 'owner');
+printTreeWithParam(newTree, 'owner');
+console.log(countAll(tree));
+console.log(countFiles(tree));
+console.log(countDirectories(tree));
+
